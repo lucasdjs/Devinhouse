c1433fb9dbc30d0ec1bd7d0687ce5a63dbc2d525
Lucas de Jesus <lucasdjs2014@hotmail.com>
2022-03-28 00:54:40 -0300

    Exercicio 9

---
+++ 01 - HTML e CSS/Semana 4/Exercicios da Semana/Exercicio 9.js
@@ -0,0 +1,14 @@
+const cidades = [
+    { nome: 'Patos de Minas', populacao: 153585 },
+    { nome: 'Lages', populacao: 157349 },
+    { nome: 'Ibiúna', populacao: 79479 },
+    { nome: 'Maringá', populacao: 403063 },
+    { nome: 'Curitiba', populacao: 1963726 },
+    { nome: 'Florianópolis', populacao: 508826 },
+    { nome: 'Pato Branco', populacao: 84779 },
+    ];
+
+    const filtroCidade = cidades.some((cidade) => {
+        return  cidade.nome.toLocaleLowerCase() === "florianópolis";
+      });
+      console.log(filtroCidade);
