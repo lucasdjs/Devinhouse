40d8784b7ec1aa1d64d318a082ef49bcea05039d
Lucas de Jesus <lucasdjs2014@hotmail.com>
2022-04-11 13:09:09 -0300

    Exercicio 1 S6

---
+++ 01 - HTML e CSS/Semana 6/Exercicios da semana/Ex 1/index.js
@@ -0,0 +1,4 @@
+function teste(){
+    var titulo = document.getElementsByTagName('h1')[0];
+    titulo.textContent = 'Titulo da página alterada';
+}
---
+++ 01 - HTML e CSS/Semana 6/Typescript/projeto-ts/src/index.ts
@@ -0,0 +1,3 @@
+const myName = "Lucas Silva";
+myName.toString = 1;
+console.log(myName);
