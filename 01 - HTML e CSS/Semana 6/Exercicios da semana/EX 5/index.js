d4ca6e30bb1c2d717866fd43ab9be6a202138f0f
Lucas de Jesus <lucasdjs2014@hotmail.com>
2022-04-18 21:26:51 -0300

    angular

---
+++ 01 - HTML e CSS/Semana 6/Exercicios da semana/EX 5/index.js
@@ -0,0 +1,28 @@
+async function fetchAPI(url, metodo){
+    const response = await fetch(url,{method:metodo});
+    return await response.json();
+
+}
+
+async function Buscar(){
+    
+    const  cep = document.getElementById('busca').value;
+
+    if(cep === ''){
+        alert('Campo precisa ser preenchido')
+    }
+    else if(cep.length !==8){
+        alert('O cep digitado está invalido')
+    }
+    else{
+        const response = await fetchAPI(`https://viacep.com.br/ws/${cep}/json/`, "GET");
+        if (response.ok) {
+            console.log(response);
+          }
+          else{
+          console.log('Deu erro');
+          }
+         
+ 
+    }
+}
