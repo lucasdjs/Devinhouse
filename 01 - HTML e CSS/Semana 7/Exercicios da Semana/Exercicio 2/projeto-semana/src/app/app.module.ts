c6ba369ccf17f1b545c7377cb8f7d2ccd382fc4b
Lucas de Jesus <lucasdjs2014@hotmail.com>
2022-04-24 22:05:14 -0300

    Exercicio 6 S8

--- 01 - HTML e CSS/Semana 7/Exercicios da Semana/Exercicio 2/projeto-semana/src/app/app.module.ts
+++ 01 - HTML e CSS/Semana 7/Exercicios da Semana/Exercicio 2/projeto-semana/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ 01 - HTML e CSS/Semana 7/Exercicios da Semana/Exercicio 2/projeto-semana/src/app/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'NGF-footer',
+  templateUrl: './footer.component.html',
+  
+  styles: [
+  ]
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
