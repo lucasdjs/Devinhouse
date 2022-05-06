841c9dd7c5f3373619fe2986422a31dc23700ce7
Lucas de Jesus <lucasdjs2014@hotmail.com>
2022-05-06 02:05:39 -0300

    EX 02 S09

---
+++ 01 - HTML e CSS/Semana 8/Exercicio da Semana/EX 02/Exercicio02.js
@@ -0,0 +1,5 @@
+var somar = function (number01, number02) {
+    var sum;
+    return console.log(sum = number01 + number02);
+};
+somar(60, 30);
---
+++ 01 - HTML e CSS/Semana 8/Exercicio da Semana/EX 02/Exercicio02.ts
@@ -0,0 +1,5 @@
+let somar = (number01:number, number02:number) =>{
+let sum:number;
+return console.log(sum = number01 +number02);
+}
+somar(30,30)
