b521d9b944d85c1a9145a5446fc954b850dbcbab
Lucas de Jesus <lucasdjs2014@hotmail.com>
2022-05-06 01:39:03 -0300

    EX01 S9

---
+++ 01 - HTML e CSS/Semana 8/Exercicio da Semana/EX 01/Exercicio01.ts
@@ -0,0 +1,6 @@
+function exercicio(){
+    let exampleLet: number = 23;
+    const exampleConst: string = "Lucas";
+    var exampleVar:string = "Olá meu nome é" +exampleConst +"e tenho" +exampleLet+"anos.";
+}
+
