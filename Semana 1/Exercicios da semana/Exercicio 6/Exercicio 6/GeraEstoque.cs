124020e01f9a17bef23e1dfc90782e7dd1dfe45c
Lucas de Jesus <lucasdjs2014@hotmail.com>
2022-06-14 01:14:49 -0300

    EX 6 S1

---
+++ Semana 1/Exercicios da semana/Exercicio 6/Exercicio 6/GeraEstoque.cs
@@ -0,0 +1,20 @@
+﻿namespace GeraEstoque
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seja bem vindo ao GeraEstoque 1.0");
+            Console.WriteLine("---------------------------------------------------");
+
+            Console.WriteLine("1  Cadastrar Produto");
+            Console.WriteLine("2  Consultar produto");
+            Console.WriteLine("3  Modificar produto");
+            Console.WriteLine("4  Excluir produto");
+            Console.WriteLine("0  Sair");
+
+            Console.Write("Digite a opção: ");
+           int opcao = int.Parse(Console.ReadLine());
+        }
+    }
+}
