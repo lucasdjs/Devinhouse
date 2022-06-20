84c6898b929c2851a65626f009ae45792ec58be9
Lucas de Jesus <lucasdjs2014@hotmail.com>
2022-06-20 02:13:48 -0300

    GeraEstoque V1 EX2

---
+++ Semana 1/Exercicios da semana/Projeto_S2/Exercicio 6/ListaProdutoScreen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_6
+{
+    internal class Class1
+    {
+    }
+}
