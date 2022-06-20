854ba59991b16d6a3b8a3abdf47c3fa02554f544
Lucas de Jesus <lucasdjs2014@hotmail.com>
2022-06-20 01:27:51 -0300

    EX 09 Projeto aplicado 5

--- Semana 1/Exercicios da semana/Projeto_S2/Exercicio 6/GeraEstoque.cs
+++ Semana 1/Exercicios da semana/Projeto_S2/Exercicio 6/GeraEstoque.cs
@@ -33,18 +33,31 @@ namespace GeraEstoque
         }
 
         public static void CadastrarProduto()
-        {
+        {   
+            Guid id = Guid.NewGuid();
+
             Console.Write("Digite o nome do produto: ");
             string nome = Console.ReadLine();
 
             Console.Write("Quantidade: ");
             int quantidade = int.Parse(Console.ReadLine());
 
-            Console.Write("Valor de compra: ");
+            Console.Write("Valor de compra: R$");
             double valorCompra = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.Write("Valor de venda: ");
+            Console.Write("Valor de venda: R$");
             double valorVenda = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Produto cadastrado com sucesso!");
+
+
+            Console.WriteLine("ID: " +id);
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Quantidade: " + quantidade);
+            Console.WriteLine("Preço de compra: R$" + valorCompra.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Preço de venda: R$" + valorVenda.ToString("F2", CultureInfo.InvariantCulture));
         }
 
        
