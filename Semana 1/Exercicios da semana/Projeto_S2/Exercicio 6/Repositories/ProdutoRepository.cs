ba9c84a48dd845df45f8777eb6b036bfc72f2972
Lucas JESUS SILVA <lucas.jesus@ndd.tech>
2022-07-05 02:37:54 -0300

    EX 07 M2S3

---
+++ Semana 1/Exercicios da semana/Projeto_S2/Exercicio 6/Repositories/ProdutoRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeraEstoque.Models;
+
+namespace GeraEstoque.Repositories
+{
+    public class ProdutoRepository
+    {
+        public IList Produtos { get; private set; }
+
+        public ProdutoRepository(IList produtos)
+        {
+            Produtos = produtos;
+        }
+    }
+}
